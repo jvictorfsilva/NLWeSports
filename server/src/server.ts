af2a07cfe31973a8a593da34c1e5d3ae72ee0a96
jvbibito12 <jvictorfsilva@gmail.com>
2022-09-15 19:17:32 -0300

    Aula 02

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,16 @@
-import express from "express";
+import express, { request } from "express";
 
 const app = express();
 
-app.get("/ads", (resquest, response) => {
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (resquest, response) => {
+  // const gameId = request.params.id;
   return response.json([
     { id: 1, name: "Anuncio 1" },
     { id: 2, name: "Anuncio 2" },
@@ -10,6 +18,11 @@ app.get("/ads", (resquest, response) => {
     { id: 4, name: "Anuncio 4" },
     { id: 5, name: "Anuncio 5" },
   ]);
+});
+
+app.get("/ads/:id/discord", (resquest, response) => {
+  // const adId = request.params.id;
+  return response.json([]);
 });
 
 app.listen(3333);
