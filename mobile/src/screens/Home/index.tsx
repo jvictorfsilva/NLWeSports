f3d2212a1c5a65f72eca6763ff6aefa26a3114af
jvbibito12 <jvictorfsilva@gmail.com>
2022-09-15 18:49:04 -0300

    Aula 02

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { View, Image } from "react-native";
+import { View, Image, FlatList } from "react-native";
 
-import { logoImg } from "../../assets/logo-nlw-esports.png";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
-
+import { GAMES } from "../../utils/games";
 import { styles } from "./styles";
 
 export function Home() {
@@ -14,6 +14,14 @@ export function Home() {
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
+      />
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
       />
     </View>
   );
